8bcbe0c3cb963552c1a1eb9abd72e8b0f29c242f
Dustin Venable <Venny44@yahoo.com>
2020-10-22 15:00:22 -0500

    updated

--- action.js
+++ action.js
@@ -36,3 +36,43 @@
 //     }
 //     return array;
 // }
+// Logarithmic Time - 
+
+
+//5 - function naiveSearch(array, item) {
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] === item) {
+//             return i;
+//         }
+//     }
+// }
+// Logarithmic Time - 
+
+
+//6 - function createPairs(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         for(let j = i + 1; j < arr.length; j++) {
+//             console.log(arr[i] + ", " +  arr[j] );
+//         }
+//     }
+// }
+// Polynomial Time - Nested Loop
+
+
+//7 - function compute(num) {
+//     let result = [];
+//     for (let i = 1; i <= num; i++) {
+
+//         if (i === 1) {
+//             result.push(0);
+//         }
+//         else if (i === 2) {
+//             result.push(1);
+//         }
+//         else {
+//             result.push(result[i - 2] + result[i - 3]);
+//         }
+//     }
+//     return result;
+// }
+// Linear Time
